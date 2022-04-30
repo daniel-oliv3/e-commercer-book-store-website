36f6b1ad996e5a969dfeb0f6cf8426a61bcda686
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-30 01:38:24 -0300

    Update login close

--- js/script.js
+++ js/script.js
@@ -6,7 +6,16 @@ document.querySelector('#search-btn').onclick = () => {
 }
 
 
+/* Login-Btn */
+let loginForm = document.querySelector('.login-form-container');
 
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
 
 
 /*  */
