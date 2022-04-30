782cd8492d9798cc6101aa2df040b27838d1376a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-30 20:33:04 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -91,3 +91,36 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+
+
+
+
+
+
+
+
+
+
+
+/*Slider Swiper Novas Chegadas/Arrivals*/
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
