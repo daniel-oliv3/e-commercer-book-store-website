a86af7defb169dc941ba9c5f40a7a3911dd5edd7
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-30 12:03:52 -0300

    Update Swiper

--- js/script.js
+++ js/script.js
@@ -42,17 +42,22 @@ window.onload = () => {
 
 
 /*Slider Swiper*/
-var swiper = new Swiper('.books-slider', {
-    loop: true,
-    breakpointts: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
     },
-});
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
