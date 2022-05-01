f4364aad3f5cc45abb6524e9657d2967bb15863c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-30 21:49:42 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -124,3 +124,55 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+
+
+
+/*Slider Swiper Reviews/Reviews*/
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
