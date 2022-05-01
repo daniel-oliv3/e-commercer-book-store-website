511aa723ef351e1d0ca7dcff548d60fcbde6b358
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-30 22:58:40 -0300

    Update swiper slide blogs

--- js/script.js
+++ js/script.js
@@ -153,8 +153,28 @@ var swiper = new Swiper(".books-slider", {
 
 
 
-
-
+/*Slider Swiper Blogs/Blogs*/
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 
 
 
